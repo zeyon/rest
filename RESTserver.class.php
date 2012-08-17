f72963f051a04bcd4fd22b5d9467e0fbdd991306
hnguyen@cms-it.de <hnguyen@cms-it.de@8435dcca-1772-4908-b004-87e61be55128>
2012-08-17 11:45:32 +0000

    CHG: Make "RESTserver->dispatch()" public and add optional call parameters.
    - CHG: Make "RESTserver->dispatch()" public and add optional call parameters "$command" and "$source" to allow direct invocation without an HTTP request. This may be useful for plugin development and for unit testing.
    
    
    git-svn-id: https://svn.zeyon.net/svn/kickstart/trunk/lib/rest@36 8435dcca-1772-4908-b004-87e61be55128

--- RESTserver.class.php
+++ RESTserver.class.php
@@ -59,12 +59,16 @@ abstract class RESTserver {
 	 *       Types:          int, float, bool, array, object, string
 	 */
 	public $actions = array();
+
 	/** @var string The variable name specifying the command variable (sometimes "cmd" or "do") */
 	private $cmdVar = 'do';
+
 	/** @var string The prefix for the local method definition */
 	public $prefix = 'do_';
+
 	/** @var bool Return an error message ['error': bool, 'trace': string] instead of false */
 	public $showerror = true;
+
 	/** @var bool Also adds a trace to the error message (see $showerror) */
 	public $showtrace = true;
 
@@ -83,78 +87,91 @@ abstract class RESTserver {
 	 * @return mixed
 	 */
 	public function initParam($var, $key=null, $type='string', $default=null, $required=true) {
-		if (is_null($key))
+		if ( is_null($key) )
 			$value = $var;
 		else {
-			if (!isset($var[$key]) and !array_key_exists($key, $var) )
-				if ($required)
+			if ( !isset($var[$key]) and !array_key_exists($key, $var ) )
+				if ( $required )
 					throw new Exception('Parameter "'.$key.'" not found!');
 				else
 					return $this->initParam($default);
 
 			$value = $var[$key];
-			if ($value === null)
+			if ( $value === null )
 				return null;
 		}
 
 		switch ( $type ) {
-    		case 'int':
+			case 'int':
 				return (int) $value;
 
-		    case 'float':
-      			return (float) $value;
+			case 'float':
+				return (float) $value;
 
-		    case 'array':
-      			return is_array($value) ? $value : array();
+			case 'array':
+				return is_array($value) ? $value : array();
 
-		    case 'bool':
-		    	return (bool)$value;
+			case 'bool':
+				return (bool)$value;
 
 			case 'object':
-      			return is_object($value) ? $value : null;
-  		}
+				return is_object($value) ? $value : null;
+		}
 
-  		return (string)$value;
+		return (string)$value;
 	}
 
 	/**
-	 * Performs the dispatch
+	 * Executes an API function.
+	 *
+	 * @param string $command Optional. The API command to run. Default is NULL.
+	 * @param array $source Optional. An associative array mapping parameter
+	 *     names to their values. Default is NULL.
+	 * @return mixed
 	 */
-    private function dispatch() {
-        try {
-			// Check for valid commands
-			if (isset($_REQUEST[$this -> cmdVar]))
-            	$command = $_REQUEST[$this -> cmdVar];
-            else
-            	throw new Exception('No command specified!');
+	public function dispatch($command = null, array $source = null) {
+		$standaloneCall = true;
 
-            if (!isset($this -> actions[$command]))
-            	throw new Exception('Unknown command: '.$command);
+		try {
+			// Check for valid commands
+			if ( $command !== null )
+				$standaloneCall = false;
+			elseif ( isset($_REQUEST[$this->cmdVar]) )
+				$command = $_REQUEST[$this->cmdVar];
+			else
+				throw new Exception('No command specified!');
+
+			if ( !isset($this->actions[$command]) )
+				throw new Exception('Unknown command: '.$command);
 
 			// array(COMMAND => array(METHOD[post, get, all], FUNCTION, PARAM1, PARAM2, ...))
-            $functionSpec = $this -> actions[$command];
-            $method = strtoupper(array_shift($functionSpec));
-            $function = array_shift($functionSpec);
+			$functionSpec = $this->actions[$command];
+			$method       = strtoupper(array_shift($functionSpec));
+			$function     = array_shift($functionSpec);
 
-            switch($method) {
-            	case 'GET':
-            		$source = $_GET;
-            		break;
-            	case 'POST':
-            		$source = $_POST;
-            		break;
-            	case 'PUT':
-            	case 'DELETE':
-            		parse_str(file_get_contents('php://input'), $source);
-            		break;
-            	default:
-            		$source = $_REQUEST;
-            		break;
-            }
+			if ( $source === null ) {
+				switch( $method ) {
+					case 'GET':
+						$source = $_GET;
+						break;
+					case 'POST':
+						$source = $_POST;
+						break;
+				case 'PUT':
+				case 'DELETE':
+					parse_str(file_get_contents('php://input'), $source);
+					break;
+					default:
+						$source = $_REQUEST;
+						break;
+				}
+			} else {
+				$standaloneCall = false;
+			}
 
-            // Get the function parameters
-            $parameters = array();
-            if ( sizeof($functionSpec) > 0 ) {
+			// Get the function parameters
+			$parameters = array();
+			if ( sizeof($functionSpec) > 0 ) {
 				foreach ($functionSpec[0] as $param) {
 					if ( !is_array($param) ) {
 						$parameters[] = call_user_func($this->initParamFunction, $source, $param);
@@ -167,48 +184,57 @@ abstract class RESTserver {
 				}
 			}
 
-            $res = call_user_func_array(array($this, $this -> prefix.$function), $parameters);
+			$res = call_user_func_array(array($this, $this->prefix.$function), $parameters);
 
 			// Check if the result should be wrapped into a JSON-encoded object
 			// (by "runJSON()").
 			if ( $res instanceof RESTvoidResult )
 				return null;
 
-           	return (is_array($res) && (isset($res['result']) || isset($res['error']))) ? $res : array('result' => $res);
-        } catch (Exception $e) {
-			if ( class_exists('PermissionDeniedException') and
+			return (is_array($res) && (isset($res['result']) || isset($res['error']))) ? $res : array('result' => $res);
+
+		} catch (Exception $e) {
+			if ( !$standaloneCall and
+				 class_exists('PermissionDeniedException') and
 				 class_exists('HTTP') and
 				 (get_class($e) === 'PermissionDeniedException') and
 				 empty($_REQUEST['_no_http_code']) )
 				HTTP::sendStatusCode(403, 'Authentication Required');
 
-        	return $this -> showerror ? ($this -> showtrace ? array('error' => $e -> getMessage(), 'trace' => $this -> errorTrace($e)) : array('error' => $e -> getMessage())) :  null;
-        }
-    }
+			if ( !$this->showerror )
+				return null;
+			elseif ( $this->showtrace )
+				return array('error' => $e->getMessage(), 'trace' => $this->errorTrace($e));
+			else
+				return array('error' => $e->getMessage());
+		}
+	}
 
-    public function errorTrace($e) {
-		$strTrace = '#0: '.$e -> getMessage().'; File: '.$e -> getFile().'; Line: '.$e -> getLine()."\n";
+	public function errorTrace($e) {
+		$strTrace = '#0: '.$e->getMessage().'; File: '.$e->getFile().'; Line: '.$e->getLine()."\n";
 		$i = 1;
-		foreach ($e -> getTrace() as $v) {
-			if (!(isset($v['function']) && $v['function'] == 'errorHandle')) {
-				if (isset($v['class']))
+
+		foreach ($e->getTrace() as $v) {
+			if ( !(isset($v['function']) && $v['function'] == 'errorHandle') ) {
+				if ( isset($v['class']) )
 					$strTrace .= "#$i: ".$v['class'].$v['type'].$v['function'].'(';
-				elseif (isset($v['function']))
+				elseif ( isset($v['function']) )
 					$strTrace .= "#$i: ".$v['function'].'(';
 				else
 					$strTrace .= "#$i: ";
 
-				if (isset($v['args']) && isset($v['function'])) {
+				if ( isset($v['args']) && isset($v['function']) ) {
 					$parts = array();
 					foreach($v['args'] as $arg)
-						$parts[] = $this -> errorArg($arg);
+						$parts[] = $this->errorArg($arg);
 					$strTrace .= implode(',', $parts).') ';
 				}
-				if (isset($v['file']) && isset($v['line']))
+				if ( isset($v['file']) && isset($v['line']) )
 					$strTrace .= '; File: '.$v['file'].'; Line: '.$v['line']."\n";
 				$i++;
 			}
 		}
+
 		return $strTrace;
 	}
 
@@ -219,48 +245,48 @@ abstract class RESTserver {
 	 * @return string
 	 */
 	public function errorArg($arg, $depth=true) {
-		if (is_string($arg))
+		if ( is_string($arg) ) {
 			return('"'.str_replace("\n", '', $arg ).'"');
-		elseif (is_bool($arg))
+		} elseif ( is_bool($arg) ) {
 			return $arg ? 'true' : 'false';
-		elseif (is_object($arg))
+		} elseif ( is_object($arg) ) {
 			return 'object('.get_class($arg).')';
-		elseif (is_resource($arg))
+		} elseif ( is_resource($arg) ) {
 			return 'resource('.get_resource_type($arg).')';
-		elseif (is_array($arg)) {
+		} elseif ( is_array($arg) ) {
 			$parts = array();
-			if ($depth)
+			if ( $depth )
 				foreach ($arg as $k => $v)
-					$parts[] = $k.' => '.$this -> errorArg($v, false);
+					$parts[] = $k.' => '.$this->errorArg($v, false);
 			return 'array('.implode(', ', $parts).')';
-		} elseif ($depth)
+		} elseif ( $depth ) {
 			return var_export($arg, true);
+		}
 	}
 
-    /**
-     * Dispatches the function call and returns the result as JSON string
-     *
-     * @return void
-     */
-    public function runJSON() {
-    	$res = $this -> dispatch();
-		if ($res != null) {
+	/**
+	 * Dispatches the function call and returns the result as JSON string
+	 *
+	 * @return void
+	 */
+	public function runJSON() {
+		$res = $this->dispatch();
+		if ( $res != null ) {
 			header('Content-Type: application/json');
-        	echo json_encode($res);
+			echo json_encode($res);
 		}
-    }
+	}
 
 	/**
 	 * Only runs a function, if a valid authentication token has been sent.
 	 */
 	public function run() {
-		if (!isset($_REQUEST[$this -> cmdVar]))
+		if ( !isset($_REQUEST[$this->cmdVar]) )
 			throw new Exception('No task specified.');
-
-		if (!in_array($_REQUEST[$this -> cmdVar], $this -> auth_exceptions) && !$this -> auth())
+		elseif ( !in_array($_REQUEST[$this->cmdVar], $this->auth_exceptions) && !$this->auth() )
 			throw new Exception('Authentication required.');
 
-		$this -> runJSON();
+		$this->runJSON();
 	}
 
 	/**
@@ -272,6 +298,7 @@ abstract class RESTserver {
 	public function auth() {
 		return true;
 	}
+
 }
 
 abstract class RESTserver2 extends RESTserver {
@@ -304,30 +331,30 @@ abstract class RESTserver2 extends RESTserver {
 			return null;
 
 		switch ( $type ) {
-    		case 'int':
-      			if ( is_numeric($value) )
+			case 'int':
+				if ( is_numeric($value) )
 					return (int)$value;
 				else
 					return ( $default === null ? null : (int)$default );
 
-		    case 'float':
-      			if ( is_numeric($value) )
+			case 'float':
+				if ( is_numeric($value) )
 					return (float)$value;
 				else
 					return ( $default === null ? null : (float)$default );
 
-		    case 'array':
-      			return is_array($value) ? $value : array();
+			case 'array':
+				return is_array($value) ? $value : array();
 
-		    case 'bool':
-		    	return (bool)$value;
+			case 'bool':
+				return (bool)$value;
 
 			case 'object':
-      			return is_object($value) ? $value : null;
+				return is_object($value) ? $value : null;
 
-  		}
+		}
 
-  		return (string)$value;
+		return (string)$value;
 	}
 
 }
