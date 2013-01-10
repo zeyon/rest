d816ce0ac8c346aedd0595c6028f107f7b4b950b
hnguyen@cms-it.de <hnguyen@cms-it.de@8435dcca-1772-4908-b004-87e61be55128>
2013-01-10 16:02:22 +0000

    FIX: "RESTclient->request()": Add missing parentheses; reformat code.
    git-svn-id: https://svn.zeyon.net/svn/kickstart/trunk/lib/rest@51 8435dcca-1772-4908-b004-87e61be55128

--- RESTclient.class.php
+++ RESTclient.class.php
@@ -16,38 +16,39 @@ class RESTclient {
 	private $header = array();
 	private $methods = array('GET', 'POST', 'PUT', 'DELETE');
 	private $method = 'GET';
+
 	public $protocol_version = false; // Use the default protocol version
 
 	public function __construct($url=null, $user=null, $password=null) {
-		$this -> setURL($url);
-		$this -> setAuthentication($user, $password);
+		$this->setURL($url);
+		$this->setAuthentication($user, $password);
 	}
 
 	/* --------------- Setting functions --------------- */
 
 	public function setURL($url) {
-		$this -> url = $url;
+		$this->url = $url;
 	}
 
 	public function setAuthentication($user, $password) {
-		$this -> user = $user;
-		$this -> password = $password;
+		$this->user = $user;
+		$this->password = $password;
 	}
 
 	public function appendHeader($strHeader) {
-		$this -> header[] = preg_replace('#$#', '', $strHeader);
+		$this->header[] = preg_replace('#$#', '', $strHeader);
 	}
 
 	public function setHeader($arrHeader) {
-		if (is_array($arrHeader))
-			$this -> header = $arrHeader;
+		if ( is_array($arrHeader) )
+			$this->header = $arrHeader;
 		else
 			throw new Exception('Invalid variable type. Expecting array.');
 	}
 
 	public function setMethod($method) {
-		if (in_array($method = strtoupper($method), $this -> methods))
-			$this -> method = $method;
+		if ( in_array($method = strtoupper($method), $this->methods) )
+			$this->method = $method;
 		else
 			return false;
 		return true;
@@ -56,103 +57,107 @@ class RESTclient {
 	/* --------------- Getting functions --------------- */
 
 	public function getURL() {
-		return $this -> url;
+		return $this->url;
 	}
 
 	public function getHeader() {
-		return implode("\r\n", $this -> header);
+		return implode("\r\n", $this->header);
 	}
 
 	/* --------------- Request functions --------------- */
 
 	public function request($params=null, $url=null, $method=null, $contenttype='text/plain', $user=null, $password=null) {
 		// Initialize parameters
-		$url = isset($url) ? $url : $this -> url;
+		$url = isset($url) ? $url : $this->url;
 		$url = parse_url($url);
 		$query = isset($url['query']) ? $url['query'] : null;
-		if (is_array($params))
+		if ( is_array($params) )
 			$query = (is_null($query) ? '' : '&').http_build_query($params, null, '&');
-		$method = strtoupper(is_null($method) ? $this -> method : $method);
-		$user = isset($url['username']) ? $url['username'] : (is_null($user) ? $this -> user : $user);
+		$method = strtoupper(is_null($method) ? $this->method : $method);
+		$user = isset($url['username']) ? $url['username'] : (is_null($user) ? $this->user : $user);
 		$password = isset($url['password']) ? $url['password'] : ( is_null($password) ? $this->password : $password );
-		if (is_null($user) || is_null($password))
+		if ( is_null($user) || is_null($password) )
 			$auth = false;
 		else
 			$auth = base64_encode($user.':'.$password);
 
-		$this -> appendHeader('Content-Type: '.$contenttype);
+		$this->appendHeader('Content-Type: '.$contenttype);
 
 		// Perform the request
-		if (in_array($method, $this -> methods)) {
-			if ($method == 'GET') {
-				// Get requests do not require a stream
-				return file_get_contents($url['scheme'].'://'.($auth == '' ? '' : $auth.'@').$url['host'].( isset($url['port']) ? ':'.$url['port'] : '' ).$url['path']
-										.(isset($query) ? '?'.$query : '')
-										.(isset($url['fragment']) ? '#'.$url['fragment'] : ''));
-			} else {
-				// In all other cases perform the Request using a stream
-				if ($auth)
-					$this -> appendHeader("Authorization: Basic $auth");
-				$this->appendHeader('Content-Length: '.strlen($query));
-				// TIPP: Add this to your script if you ever encounter the ugly "417 - Expectation Failed" error message
-				//$this->appendHeader('Expect:');
-				$ctx = stream_context_create(
-					array('http' => array(
-						'method' => $method,
-						'header'=> $this -> getHeader(),
-						'content' => $query
-						) + $this->protocol_version ? array('protocol_version' => $this->protocol_version) : array()
-					)
-				);
-				return file_get_contents($url['scheme'].'://'.$url['host'].( isset($url['port']) ? ':'.$url['port'] : '' ).(isset($url['path']) ? $url['path'] : '').(isset($url['fragment']) ? '#'.$url['fragment'] : ''), false, $ctx);
-			}
-		} else
+
+		if ( !in_array($method, $this->methods) )
 			throw new Exception('Invalid HTTP method: '.$method);
+
+		if ( $method == 'GET' ) {
+			// Get requests do not require a stream
+			return file_get_contents($url['scheme'].'://'.($auth == '' ? '' : $auth.'@').$url['host'].( isset($url['port']) ? ':'.$url['port'] : '' ).$url['path']
+									.(isset($query) ? '?'.$query : '')
+									.(isset($url['fragment']) ? '#'.$url['fragment'] : ''));
+		} else {
+			// In all other cases perform the Request using a stream
+			if ( $auth )
+				$this->appendHeader('Authorization: Basic '.$auth);
+
+			$this->appendHeader('Content-Length: '.strlen($query));
+
+			// Add this to your script if you ever encounter an
+			// "417 - Expectation Failed" error message.
+			//$this->appendHeader('Expect:');
+
+			$ctx = stream_context_create(array(
+				'http' => array(
+					'method'  => $method,
+					'header'  => $this->getHeader(),
+					'content' => $query
+				) + ( $this->protocol_version ? array('protocol_version' => $this->protocol_version) : array() )
+			));
+			return file_get_contents($url['scheme'].'://'.$url['host'].( isset($url['port']) ? ':'.$url['port'] : '' ).(isset($url['path']) ? $url['path'] : '').(isset($url['fragment']) ? '#'.$url['fragment'] : ''), false, $ctx);
+		}
 	}
 
      /**
       * Convenience method wrapping a commom POST call
       */
      public function post($params=null, $url=null, $contenttype='application/x-www-form-urlencoded', $user=null, $password=null) {
-		return $this -> request($params, $url, 'POST', $contenttype, $user, $password);
+		return $this->request($params, $url, 'POST', $contenttype, $user, $password);
      }
 
      /**
       * Convenience method wrapping a commom PUT call
       */
      public function put($params=null, $url=null, $contenttype=null, $user=null, $password=null) {
-		return $this -> request($params, $url, 'PUT', $contenttype, $user, $password);
+		return $this->request($params, $url, 'PUT', $contenttype, $user, $password);
      }
 
      /**
       * Convenience method wrapping a commom GET call
       */
      public function get($params=null, $url=null, $contenttype=null, $user=null, $password=null) {
-		return $this -> request($params, $url, 'GET', $contenttype, $user, $password);
+		return $this->request($params, $url, 'GET', $contenttype, $user, $password);
      }
 
      /**
       * Convenience method wrapping a commom delete call
       */
      public function delete($params=null, $url=null, $contenttype=null, $user=null, $password=null) {
-		return $this -> request($params, $url, 'DELETE', $contenttype, $user, $password);
+		return $this->request($params, $url, 'DELETE', $contenttype, $user, $password);
      }
-     
+
 	/**
 	 * Initializes and checks a server result
-	 * 
+	 *
 	 * @param array $res
 	 */
 	public static function initResult($res) {
-		if (!is_array($res))
+		if ( !is_array($res) )
 			throw new Exception('Invalid datatype. Array expected!');
-		
-		if (isset($res['error']))
+
+		if ( isset($res['error']) )
 			throw new Exception('Server error: '.$res['error']);
-			
-		if (!isset($res['result']))
+
+		if ( !isset($res['result']) )
 			throw new Exception('Server returned no result: '.$ser);
-			
+
 		return $res['result'];
 	}
 }
